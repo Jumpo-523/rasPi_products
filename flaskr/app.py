7f0e00d71191e952e0af496d11e2b87116714898
j.takubo <nyk523jt@gmail.com>
2020-10-22 07:54:07 +0900

    ホスト名を指定していな買った

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,4 @@
-
+import argparse
 # __init__ファイル内の関数は,相対参照している。より良い方法があるか？...
 from . import create_app
 # from flask_socketio import SocketIO, emit, join_room, leave_room, \
@@ -30,4 +30,13 @@ thread = None
 
 if __name__ == '__main__':
     # socketio.run(app, debug=True)
-    app.run(debug=True,host="0.0.0.0", port=5000)
+    # herokuが引数を
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=int(os.environ.get('PORT', 8000)), help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    arg_parser.add_argument('--host', default='0.0.0.0', help='host')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, host=options.host, port=options.port)
