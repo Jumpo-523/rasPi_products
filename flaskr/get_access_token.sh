e7d92ab00334c5e0a049111ee08b9ba3255b3395
j.takubo <nyk523jt@gmail.com>
2020-10-26 20:00:34 +0900

    delete secret info

--- flaskr/get_access_token.sh
+++
@@ -1,5 +0,0 @@
-curl -v -X POST https://api.line.me/v2/oauth/accessToken \
--H "Content-Type: application/x-www-form-urlencoded" \
---data-urlencode 'grant_type=client_credentials' \
---data-urlencode 'client_id=1655108566' \
---data-urlencode 'client_secret=8352edca599153f2362bc9b7d3bb2610'
